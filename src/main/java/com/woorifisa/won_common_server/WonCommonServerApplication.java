c6699939fc5fa72906f6dc34bc1fec65c2370bf8
Chae-Yu <chaeyu2004@naver.com>
2026-05-15 11:45:10 +0900

    Setting: Spring Boot 초기 세팅 설정

---
+++ src/main/java/com/woorifisa/won_common_server/WonCommonServerApplication.java
@@ -0,0 +1,13 @@
+package com.woorifisa.won_common_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonCommonServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WonCommonServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woorifisa/won_common_server/WonCommonServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woorifisa.won_common_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonCommonServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

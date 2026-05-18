dac699090dbae2c76dddd5a2dd05f9b9bd59d04f
Chae-Yu <chaeyu2004@naver.com>
2026-05-18 17:37:18 +0900

    Feat: CommUser, CommUserMapping 엔터티 클래스 추가

---
+++ src/main/java/com/woorifisa/won_common_server/domain/mapping/model/CommUser.java
@@ -0,0 +1,30 @@
+package com.woorifisa.won_common_server.domain.mapping.model;
+
+import com.woorifisa.won_common_server.global.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.type.SqlTypes;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Table(name = "comm_user")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CommUser extends BaseTimeEntity {
+    @Id
+    @UuidGenerator
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    @Column(name = "user_uuid", columnDefinition = "VARCHAR(36)")
+    private UUID userUuid;
+
+    @Column(name = "ci_hash", length = 255, unique = true)
+    private String ciHash;
+}

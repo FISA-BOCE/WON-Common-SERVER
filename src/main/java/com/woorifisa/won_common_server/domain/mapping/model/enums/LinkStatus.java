460d77cd0de4752a0f994242ffb9a6a1fc979bf3
Chae-Yu <chaeyu2004@naver.com>
2026-05-19 17:14:41 +0900

    Feat: 연결 상태 여부 반환 메서드 추가

--- src/main/java/com/woorifisa/won_common_server/domain/mapping/model/enums/LinkStatus.java
+++ src/main/java/com/woorifisa/won_common_server/domain/mapping/model/enums/LinkStatus.java
@@ -2,4 +2,8 @@ package com.woorifisa.won_common_server.domain.mapping.model.enums;
 
 public enum LinkStatus {
     NONE, LINKED, UNLINKED;
+
+    public boolean isConnected() {
+        return this == LINKED;
+    }
 }

da1393be29d5fbcbf63c278ecb19e1f3d51eb218
Chae-Yu <chaeyu2004@naver.com>
2026-05-20 10:31:52 +0900

    Fix: CommUserRepository ID 타입 수정
    - CommUser 엔티티의 PK 타입에 맞춰 Repository ID 타입을 UUID로 변경

--- src/main/java/com/woorifisa/won_common_server/domain/mapping/repository/CommUserMappingRepository.java
+++ src/main/java/com/woorifisa/won_common_server/domain/mapping/repository/CommUserMappingRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface CommUserMappingRepository extends JpaRepository<CommUserMapping, Long> {
+public interface CommUserMappingRepository extends JpaRepository<CommUserMapping, UUID> {
     Optional<CommUserMapping> findByCommUserUserUuid(UUID userUuid);
 }
--- src/main/java/com/woorifisa/won_common_server/domain/mapping/repository/CommUserRepository.java
+++ src/main/java/com/woorifisa/won_common_server/domain/mapping/repository/CommUserRepository.java
@@ -3,5 +3,7 @@ package com.woorifisa.won_common_server.domain.mapping.repository;
 import com.woorifisa.won_common_server.domain.mapping.model.CommUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommUserRepository extends JpaRepository<CommUser, Long> {
+import java.util.UUID;
+
+public interface CommUserRepository extends JpaRepository<CommUser, UUID> {
 }

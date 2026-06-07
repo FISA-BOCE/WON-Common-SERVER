44954795927862ca48b4994601ebb584872607a5
chaeyu <chaeyu2004@naver.com>
2026-06-07 19:52:52 +0900

    Chore: 401 에러 코드 추가

--- src/main/java/com/woorifisa/won_common_server/global/exception/code/CommonErrorCode.java
+++ src/main/java/com/woorifisa/won_common_server/global/exception/code/CommonErrorCode.java
@@ -7,6 +7,8 @@ public enum CommonErrorCode implements ErrorCode {
     INVALID_REQUEST(HttpStatus.BAD_REQUEST, "COM_400_001", "요청 형식이 올바르지 않습니다."),
     INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "COM_400_002", "유효하지 않은 요청값입니다."),
 
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COM_401_001", "인증이 필요합니다."),
+
     RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "COM_404_001", "요청한 데이터를 찾을 수 없습니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COM_500_001", "서버 내부 오류가 발생했습니다.");
 
